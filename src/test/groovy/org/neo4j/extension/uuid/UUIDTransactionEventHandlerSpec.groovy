11c6777231d6ca595f6fc0fcde90bee62238e8a7
Stefan Armbruster <stefan@armbruster-it.de>
2012-08-08 23:12:04 +0200

    adding Spock test using Neo4j (supposed to fail for now)

---
+++ src/test/groovy/org/neo4j/extension/uuid/UUIDTransactionEventHandlerSpec.groovy
@@ -0,0 +1,27 @@
+package org.neo4j.extension.uuid
+
+import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.test.ImpermanentGraphDatabase
+import spock.lang.Specification
+
+class UUIDTransactionEventHandlerSpec extends Specification {
+
+    def "check if a new node has a uuid"() {
+        setup:
+        GraphDatabaseService graphDB =  new ImpermanentGraphDatabase()
+
+        when:
+        def tx = graphDB.beginTx()
+        def node = null
+        try {
+            node = graphDB.createNode()
+            tx.success()
+        } finally {
+            tx.finish()
+        }
+
+        then:
+        node.hasProperty('uuid')
+
+    }
+}
