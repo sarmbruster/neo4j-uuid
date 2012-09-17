ecdea257ca6b5f33796f895468efc4c6a7ce345b
Stefan Armbruster <stefan@armbruster-it.de>
2012-09-17 18:42:48 +0200

    new: autoindex uuid, support uuid on relationship, check for modification attempts * added unit tests * tests use ServerBuilder

--- src/main/java/org/neo4j/extension/uuid/UUIDTransactionEventHandler.java
+++ src/main/java/org/neo4j/extension/uuid/UUIDTransactionEventHandler.java
@@ -1,30 +1,85 @@
 package org.neo4j.extension.uuid;
 
+import org.apache.commons.configuration.Configuration;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.PropertyContainer;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.event.PropertyEntry;
 import org.neo4j.graphdb.event.TransactionData;
 import org.neo4j.graphdb.event.TransactionEventHandler;
+import org.neo4j.graphdb.index.AutoIndexer;
+import org.neo4j.graphdb.index.IndexManager;
+import org.neo4j.server.plugins.Injectable;
+import org.neo4j.server.plugins.PluginLifecycle;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.UUID;
 
-class UUIDTransactionEventHandler implements TransactionEventHandler {
+//@Path("uuid")
+public class UUIDTransactionEventHandler implements TransactionEventHandler, PluginLifecycle {
 
     public static final String UUID_PROPERTY_NAME = "uuid";
 
-    @java.lang.Override
+    @Override
     public Object beforeCommit(TransactionData data) throws Exception {
-        for (Node node : data.createdNodes()) {
-            if (!node.hasProperty(UUID_PROPERTY_NAME)) {
-                node.setProperty(UUID_PROPERTY_NAME, UUID.randomUUID().toString());
-            }
-        }
+
+        checkForUuidChanges(data.removedNodeProperties(), "remove");
+        checkForUuidChanges(data.assignedNodeProperties(), "assign");
+        checkForUuidChanges(data.removedRelationshipProperties(), "remove");
+        checkForUuidChanges(data.assignedRelationshipProperties(), "assign");
+
+        populateUuidsFor(data.createdNodes());
+        populateUuidsFor(data.createdRelationships());
+
         return null;
     }
 
-    @java.lang.Override
+    @Override
     public void afterCommit(TransactionData data, java.lang.Object state) {
     }
 
-    @java.lang.Override
+    @Override
     public void afterRollback(TransactionData data, java.lang.Object state) {
     }
+
+    @Override
+    public Collection<Injectable<?>> start(GraphDatabaseService graphDatabaseService, Configuration config) {
+        IndexManager indexManager = graphDatabaseService.index();
+        setupUUIDIndexing(indexManager.getNodeAutoIndexer());
+        setupUUIDIndexing(indexManager.getRelationshipAutoIndexer());
+        graphDatabaseService.registerTransactionEventHandler(this);
+        return Collections.emptySet();
+    }
+
+    @Override
+    public void stop() {
+        // intentionally empty
+    }
+
+    /**
+     * @param propertyContainers set UUID property for a iterable on nodes or relationships
+     */
+    private void populateUuidsFor(Iterable<? extends PropertyContainer> propertyContainers) {
+        for (PropertyContainer propertyContainer : propertyContainers) {
+            if (!propertyContainer.hasProperty(UUID_PROPERTY_NAME)) {
+                propertyContainer.setProperty(UUID_PROPERTY_NAME, UUID.randomUUID().toString());
+            }
+        }
+    }
+
+    private void checkForUuidChanges(Iterable<? extends PropertyEntry<? extends PropertyContainer>> changeList, String action) {
+        for (PropertyEntry<? extends PropertyContainer> removedProperty : changeList) {
+            if (removedProperty.key().equals(UUID_PROPERTY_NAME)) {
+                throw new IllegalStateException("you are not allowed to " + action + " " + UUID_PROPERTY_NAME + " properties");
+            }
+        }
+    }
+
+    private void setupUUIDIndexing(AutoIndexer<? extends PropertyContainer> autoIndexer) {
+        autoIndexer.startAutoIndexingProperty(UUID_PROPERTY_NAME);
+        autoIndexer.setEnabled(true);
+    }
+
 }
--- src/test/groovy/org/neo4j/extension/uuid/UUIDTransactionEventHandlerSpec.groovy
+++ src/test/groovy/org/neo4j/extension/uuid/UUIDTransactionEventHandlerSpec.groovy
@@ -1,28 +1,145 @@
 package org.neo4j.extension.uuid
 
 import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.graphdb.Transaction
+import org.neo4j.graphdb.Node
 import org.neo4j.test.ImpermanentGraphDatabase
+import spock.lang.Shared
 import spock.lang.Specification
+import org.neo4j.server.helpers.ServerBuilder
+import org.neo4j.server.NeoServer
+import org.neo4j.server.plugins.PluginLifecycle
+import org.neo4j.graphdb.DynamicRelationshipType
+import org.neo4j.graphdb.TransactionFailureException
 
 class UUIDTransactionEventHandlerSpec extends Specification {
 
+    @Shared GraphDatabaseService graphDB
+    @Shared NeoServer server
+    Transaction tx
+
+    def setupSpec() {
+        server = ServerBuilder.server().withThirdPartyJaxRsPackage("org.neo4j.extension.uuid", "/uuid").build();
+        server.start()
+        graphDB = server.database.graph
+    }
+
+    def cleanupSpec() {
+        server.stop()
+    }
+
+    /*def setup() {
+        tx = graphDB.beginTx()
+
+    } */
+
+    def "check ServiceLoader works for PluginLifecycle"() {
+        when:
+        def services = ServiceLoader.load(PluginLifecycle)
+        then:
+        services.iterator().size() > 0
+    }
+
     def "check if a new node has a uuid"() {
         setup:
-        GraphDatabaseService graphDB =  new ImpermanentGraphDatabase()
-        graphDB.registerTransactionEventHandler(new UUIDTransactionEventHandler())
+        def node = withTransaction { graphDB.createNode() }
 
         when:
+        def uuid = node.getProperty('uuid', null)
+
+        then:
+        uuid =~ /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/
+    }
+
+    def "check if a new relationship has a uuid"() {
+        setup:
+        def rel = withTransaction {
+            def startNode = graphDB.createNode()
+            def endNode = graphDB.createNode()
+            startNode.createRelationshipTo(endNode, DynamicRelationshipType.withName('RELATED'))
+        }
+
+        when:
+        def uuid = rel.getProperty('uuid', null)
+
+        then:
+        uuid =~ /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/
+    }
+
+
+    def "verify autoindexing on uuids"() {
+        setup:
+        def node = withTransaction { graphDB.createNode() }
+
+        when:
+        def hits = graphDB.index().nodeAutoIndexer.autoIndex.get('uuid', node.getProperty('uuid'))
+
+        then:
+        hits.size()==1
+        hits.single == node
+    }
+
+    def "check if changes to uuid are rejected"() {
+        setup:
+        def startNode
+        def rel
+        withTransaction {
+                    startNode = graphDB.createNode()
+                    def endNode = graphDB.createNode()
+                    rel = startNode.createRelationshipTo(endNode, DynamicRelationshipType.withName('RELATED'))
+                }
+
+        when: "trying to change uuid on node"
+        withTransaction { startNode.setProperty("uuid", "123")}
+
+        then:
+        def e = thrown(TransactionFailureException)
+        e.cause.cause.message =~ /you are not allowed to assign uuid properties/
+
+        when: "trying to remove uuid on node"
+        withTransaction { startNode.removeProperty("uuid")}
+
+        then:
+        e = thrown(TransactionFailureException)
+        e.cause.cause.message =~ /you are not allowed to remove uuid properties/
+
+        when: "trying to change uuid on relationship"
+        withTransaction { rel.setProperty("uuid", "123")}
+
+        then:
+        e = thrown(TransactionFailureException)
+        e.cause.cause.message =~ /you are not allowed to assign uuid properties/
+
+        when: "trying to remove uuid on relationship"
+        withTransaction { rel.removeProperty("uuid")}
+
+        then:
+        e = thrown(TransactionFailureException)
+        e.cause.cause.message =~ /you are not allowed to remove uuid properties/
+
+    }
+
+
+    def withTransaction(Closure closure) {
         def tx = graphDB.beginTx()
-        def node = null
         try {
-            node = graphDB.createNode()
+            def result = closure.call()
             tx.success()
+            return result
         } finally {
             tx.finish()
         }
+    }
 
-        then:
-        node.hasProperty('uuid')
-
+    Node createNode() {
+        withTransaction {graphDB.createNode()}
+        /*def tx = graphDB.beginTx()
+        try {
+            def node = graphDB.createNode()
+            tx.success()
+            node
+        } finally {
+            tx.finish()
+        } */
     }
 }
